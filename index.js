a1b0d35eb92665e2b0e8a568bbc9cb11d89b53d8
Keanu <keanutimmermans8@gmail.com>
2020-08-09 14:01:30 +0200

    Added basic toggle functionality.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const config = require(configpath);
 const tokenpath = require("./token.json")
 const os = require('os')
 const PREFIX = "%!";
+var autoReply = false
 
 var bot = new Discord.Client();
 
@@ -33,6 +34,47 @@ bot.on("ready", async function() {
     if (os.platform == "linux") console.log("I'm running on Linux...") 
     if (os.platform == "win32") console.log("I'm running on Windows...")
     console.log("Time: " + d())
+    bot.user.setStatus("online");
+});
+
+bot.on('message', async (msg) => {
+    // const guildTag =
+    //   msg.channel.type === 'text' ? `[${msg.guild.name}]` : '[DM]';
+    // const channelTag =
+    //   msg.channel.type === 'text' ? `[#${msg.channel.name}]` : '';
+    // console.log(
+    //   `${guildTag}${channelTag} ${msg.author.tag}: ${msg.content}`,
+    // );
+
+    if (msg.author.tag !== bot.user.tag) return;
+    // if (msg.guild === null) return;
+    // if (autoReply === true) {
+    //     msg.reply('unavailable.')
+    // }
+
+    if (msg.content.startsWith(`${PREFIX}test`)) {
+        msg.edit(msg.author.id);
+    }
+
+    if (msg.content.startsWith(`${PREFIX}on`)) {
+        autoReply = true;
+        msg.channel.send(`Turned AutoReply on.`);
+    }
+    
+    if (msg.content.startsWith(`${PREFIX}off`)) {
+        autoReply = false;
+        msg.channel.send(`Turned AutoReply off.`);
+    }
+
+    if (msg.content.startsWith(`${PREFIX}value`)) {
+        if (autoReply == true){
+            msg.channel.send(`AutoReply is turned on.`);
+        }
+        else {
+            msg.channel.send(`AutoReply is turned off.`);
+        }
+    }
+
 });
 
 botstartupmode();
